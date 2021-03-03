8e14e612d6d8a004fd6ef4ecd0e1f801b6eec2ec
milafreckled <miladul2014@gmail.com>
2021-03-03 17:55:22 +0100

    server set up

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-const http = require('http')
-const fs = require('fs')
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'text/html' })
-  fs.createReadStream('index.html').pipe(res)
-})
+var connect = require('connect');
+var serveStatic = require('serve-static');
 
-server.listen(process.env.PORT || 3000)
+connect()
+    .use(serveStatic(__dirname))
+    .listen(8080, () => console.log('Server running on 8080...'));
